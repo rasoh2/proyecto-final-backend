c9d87e2fb650ff3537599d45327f25a1a693e2a5
rasoh2 <sebastian.ortega.auriol@gmail.com>
2024-02-28 23:06:31 -0400

    agrega cors todos los origenes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import * as routes from './server/routes/index.js'
-import {tracker} from './server/middlewares/router.middlewares.js'
+import express from "express";
+import cors from "cors";
+import * as routes from "./server/routes/index.js";
+import { tracker } from "./server/middlewares/router.middlewares.js";
 
-const PORT = process.env.PORT ?? 3000
-const app = express ()
+const PORT = process.env.PORT ?? 3000;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-app.use(tracker)
+app.use(cors(origin(corsOptions)));
+app.use(express.json());
 
-app.use(routes.usuarios)
-app.use(routes.errors)
+app.use(tracker);
 
-app.listen(PORT, ()=> console.log(`SERVER UP in URL: http://localhost:${PORT}`))
+app.use(routes.usuarios);
+app.use(routes.errors);
+
+app.listen(PORT, () =>
+  console.log(`SERVER UP in URL: http://localhost:${PORT}`)
+);
 
 export default app;
