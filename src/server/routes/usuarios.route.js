6bae5a828df0e42bb36c832d84b01cfdc9c5f133
rasoh2 <sebastian.ortega.auriol@gmail.com>
2024-02-28 22:58:20 -0400

    added health check

--- src/server/routes/usuarios.route.js
+++ src/server/routes/usuarios.route.js
@@ -1,38 +1,40 @@
-import express from 'express';
-import * as usuarios from '../controllers/usuarios.controllers.js';
-import {authToken} from '../middlewares/cursos.middlewares.js';
+import express from "express";
+import * as usuarios from "../controllers/usuarios.controllers.js";
+import { authToken } from "../middlewares/cursos.middlewares.js";
 
 const router = express.Router();
 
 // crear usuario (ok)
-router.post('/login', usuarios.login)
+router.post("/login", usuarios.login);
 
 //router.get('/usuarios', authToken, usuarios.findUserByEmail)//xx
-router.post('/usuarios', usuarios.register)
+router.post("/usuarios", usuarios.register);
 
 // crear producto (ok)
-router.post('/productos/nuevo', usuarios.nuevoProducto)
+router.post("/productos/nuevo", usuarios.nuevoProducto);
 
 // obtener productos para gallery (ok)
-router.get('/productos', usuarios.ObtenerProductos);
-router.get('/productos/:producto_id', usuarios.ObtenerProductoId);
+router.get("/productos", usuarios.ObtenerProductos);
+router.get("/productos/:producto_id", usuarios.ObtenerProductoId);
 
 // borrar productos (ok)
-router.delete('/productos/:producto_id', usuarios.eliminarProducto);
+router.delete("/productos/:producto_id", usuarios.eliminarProducto);
 
 // editar productos (ok)
-router.put('/productos/edit/:id', usuarios.actualizarProducto)
-
+router.put("/productos/edit/:id", usuarios.actualizarProducto);
 
 //=============== VENTA =========================================
 
 // Operaciones relacionadas con el carrito
-router.post('/carrito', usuarios.agregarProductoAlCarrito);
-router.get('/carrito/:usuario_id', usuarios.obtenerProductosEnCarrito);
-router.delete('/carrito/:carrito_id', usuarios.eliminarProductoDelCarrito);
+router.post("/carrito", usuarios.agregarProductoAlCarrito);
+router.get("/carrito/:usuario_id", usuarios.obtenerProductosEnCarrito);
+router.delete("/carrito/:carrito_id", usuarios.eliminarProductoDelCarrito);
 
 // Operaciones relacionadas con las ventas
-router.post('/ventasimple', usuarios.ventaSimple);
-router.get('/ventas/:usuario_id', usuarios.obtenerVentasUsuario)
+router.post("/ventasimple", usuarios.ventaSimple);
+router.get("/ventas/:usuario_id", usuarios.obtenerVentasUsuario);
+
+// Health check
+router.get("/", (req, res) => res.status(200).send("Servidor funcionando"));
 
 export default router;
